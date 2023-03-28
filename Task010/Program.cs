2a0dd6c3bca502a92f4ae63072795c9a75049145
силивончик елена <e.silivonchik@sckkgit initgit config --global user.email e.silivonchik@sckk>
2023-03-28 22:50:14 +0300

    Решение задачи 10

---
+++ Task010/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число: ");
+
+int threedigit = int.Parse(Console.ReadLine()!);
+if(threedigit > 100 && threedigit < 1000 )
+{
+    Console.WriteLine($"Вторая цифра этого числа {threedigit / 10 % 10}");
+}
+else
+{
+    Console.WriteLine("Ошибка. Это число не трёхзначное.");
+}
